e0ccdad25912cdb85b919ecefbdd007469a712b0
Luis Felipe <lfghias232@gmail.com>
2022-02-14 21:37:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Say Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './styles'
+
+function Header() {
+  return (
+    <Container>Header</Container>
+  )
+}
+
+export default Header
