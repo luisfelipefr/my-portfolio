45e19b1d94e5543248f1b75e7f9acca0e9c7c4b4
Luis Felipe <lfghias232@gmail.com>
2022-02-20 23:04:26 -0300

    add dependencie react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Pages/Home";
 import { SocialMedia } from "./Components/SocialMedia";
 import { GlobalStyle } from "./styles/global";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <Home/>
-    </div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
