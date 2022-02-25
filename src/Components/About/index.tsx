db84503af7fb3f91971670728b247ee6b484e293
Luis Felipe <lfghias232@gmail.com>
2022-02-24 21:59:37 -0300

    Create About component

---
+++ src/Components/About/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ToggleList } from "../ToggleList";
+import { Container, Text } from "./styles";
+
+export function About() {
+  return (
+    <Container>
+      <h1>
+        <span>02.</span> About Me
+      </h1>
+      <div>
+        <Text>
+          Hello! My name is Luis, I&rsquo;m a Front-End Developer located in
+          Brazil.
+        </Text>
+        <Text>
+          I have a passion for <span>responsive, accessible</span> apps and
+          creating <span>dynamic</span> and{" "}
+          <span>intuitive user experiences.</span>
+        </Text>
+        <Text>
+          Here are a few technologies I&rsquo;ve been working with recently:
+        </Text>
+        <div className="list">
+          <ToggleList Arrow=">" Text="JavaScript(ES6+)" />
+          <ToggleList Arrow=">" Text="TypeScript" />
+          <ToggleList Arrow=">" Text="Styled-component" />
+          <ToggleList Arrow=">" Text="React Native" />
+          <ToggleList Arrow=">" Text="ReactJS" />
+        </div>
+      </div>
+    </Container>
+  );
+}
---
+++ src/Components/About/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 85.75rem;
+  height: 100vh;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 1.5rem;
+ 
+  span {
+    color: var(--green);
+  }
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 1.625rem;
+  line-height: 32px;
+  max-width: 49rem;
+  margin-top: 45px;
+`;
