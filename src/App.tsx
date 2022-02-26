46b7d054a2f960e6385fd825cb5a35206a777b68
Luis Felipe <lfghias232@gmail.com>
2022-02-25 21:56:40 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About } from "./Components/About";
 import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Pages/Home";
 import { SocialMedia } from "./Components/SocialMedia";
@@ -12,7 +13,7 @@ export function App() {
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={[<Home />, <About/>]} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Pages/Home/index.tsx
+++ src/Components/Pages/Home/index.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import { SocialMedia } from "../../SocialMedia";
 import { About } from "../../About";
-import { Background, Container, Subtitle, Title } from "./styles";
+import { Container, Subtitle, Title } from "./styles";
 
 export function Home() {
   return (
-    <>
-    <Background>
-      <Container>
+    <Container>
       <p className="styled">introducing</p>
       <Title className="titleStyled">Hello</Title>
       <Title>I'm Luis Felipe.</Title>
       <Subtitle>I build things for the web.</Subtitle>
       <p>I&#x2019;m a Front-end developer.</p>
     </Container>
-    </Background>
-    <About/>
-    </>
-    
   );
 }
