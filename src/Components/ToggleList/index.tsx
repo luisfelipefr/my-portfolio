3c500b0871d87d084cfe150e5617f607fa42a362
luisfelipert <lfghias232@gmail.com>
2022-07-10 03:59:13 -0300

    Updating stuff

--- src/Components/ToggleList/index.tsx
+++ src/Components/ToggleList/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Container, Technologies, Title } from "./styles";
 
 interface List {
-  Arrow: string;
+  Title: string;
   Text: string;
 }
 
 export function ToggleList(props: List) {
   return (
     <Container>
-      <Title>{props.Arrow}</Title>
+      <Title>{props.Title}</Title>
       <Technologies>{props.Text}</Technologies>
     </Container>
   );
--- src/Components/ToggleList/styles.ts
+++ src/Components/ToggleList/styles.ts
@@ -5,13 +5,13 @@ export const Container = styled.div`
   margin: 39px 22px 40px 0;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.span`
   color: var(--green);
   font-size: 0.875rem;
   
 `;
 
-export const Technologies = styled.h1`
+export const Technologies = styled.p`
   color: var(--shape);
   font-size: 0.875rem;
   margin-left: 13px;
