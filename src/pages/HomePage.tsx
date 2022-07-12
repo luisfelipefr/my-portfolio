077faab9ff029b2e95ba895ef28c9899071cbf26
luisfelipert <lfghias232@gmail.com>
2022-07-12 00:36:51 -0300

    Adding the contact page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Navbar } from '../Components/Navbar'
 import { About } from '../Components/Pages/About'
+import { Contact } from '../Components/Pages/Contact'
 import { Home } from '../Components/Pages/Home'
 import { Works } from '../Components/Pages/Works'
 
 export function HomePage() {
   return (
     <>
+    
       <Home/>
       <About/>
       <Works/>
+      <Contact/>
     </>
   )
 }
