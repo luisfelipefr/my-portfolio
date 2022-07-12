ebf063bf729744d66bc7902c9872ee58a03e3cc6
luisfelipert <lfghias232@gmail.com>
2022-07-12 00:29:29 -0300

    Creating a new route

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { GlobalStyle } from "./styles/global";
 import { HomePage } from "./pages/HomePage";
+import { Successfully } from "./Components/Pages/Sucessfully";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/email-sent" element={<Successfully />} />
         </Routes>
       </BrowserRouter>
     </>
