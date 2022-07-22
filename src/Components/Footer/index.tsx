92467bbe1b711d4162500f5bce40de622f652d05
luisfelipert <lfghias232@gmail.com>
2022-07-22 01:56:10 -0300

    Adding component footer

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { Lang } from "../Lang";
+import { Container } from "./styles";
+
+
+export function Footer() {
+
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
---
+++ src/Components/Footer/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface INav {
+  click: boolean;
+}
+
+export const Container =  styled.div`
+   /* margin: 0 auto;
+  max-width: 85.75rem; */
+  width: 100%;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 1.5rem;
+  box-shadow: 0px 2px 11px 2px #000;
+
+  min-height: 80px;
+`;
